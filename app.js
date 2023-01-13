f5c23dbac0c87594471876078ee0c7f8fe03f5d4
LudmilaS <ludmila.https@gmail.com>
2023-01-13 18:53:10 -0300

    conectando com o banco

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 import express from 'express'
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+import joi from "joi";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from 'uuid';
 
 const server = express()
+dotenv.config();
+server.use(cors());
+server.use(express.json());
 const PORT = 5000
+
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+let db
+
+try {
+await mongoClient.connect()
+} catch (error) {
+ console.log("Error in mongo conect", error.message)
+}
+  
+db = mongoClient.db("DIRETORIO");
+const talCollection = db.collection("COLLECTION");
 
 server.listen(PORT, () => {
   console.log('Im a server')
