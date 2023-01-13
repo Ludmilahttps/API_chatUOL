0bf45d5827579ce66c44a5a5c9ad9211d7212b7d
LudmilaS <ludmila.https@gmail.com>
2023-01-13 19:41:51 -0300

    post/participants ok

--- app.js
+++ app.js
@@ -19,26 +19,47 @@ server.use(express.json())
 const PORT = 5000
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
-let db = mongoClient.db();
+let db
 
 try {
-await mongoClient.connect()
-} catch (error) {
- console.log("Error in mongo conect", error.message)
+  await mongoClient.connect()
 }
-  
-db = mongoClient.db("DIRETORIO");
-const talCollection = db.collection("COLLECTION");
+catch (error) {
+  console.log("Error in mongo conect", error.message)
+}
 
-//* init a server
+db = mongoClient.db()
+const talCollection = db.collection("COLLECTION")
+
+// * init a server
 server.listen(PORT, () => {
   console.log('Im a server')
 })
 
-server.post('/participants', (request, response) => {
+// * POST participants
+server.post('/participants', async (request, response) => {
   console.log("post participants")
 
-  let name
-  name = request.body.name
-  response.status(201).send('OK')
+  let name = request.body.name
+  const validate = nameValid.validate(request.body)
+  if (validate.error) {
+    return response.status(422).send('Unprocessable Entity')
+  }
+
+  const OuterName = await db.collection('participants').findOne({ name })
+  if (OuterName) {
+    return response.status(422).send('Conflict ')
+  }
+
+  try {
+    await db.collection('participants').insertOne({ name, lastStatus: Date.now() })
+    await db.collection('messages').insertOne({ from: name, to: 'Todos', text: 'enter the room...', type: 'status', time: dayjs().format('HH:mm:ss') })
+    response.status(401).send('OK')
+  } catch (error) {
+    response.send("something is wrong")
+  }
+})
+
+const nameValid = joi.object({
+  name: joi.string().required()
 })
