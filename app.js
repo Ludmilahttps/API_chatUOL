aa92c672c22128d8ad8b3d3932e2b83f62d7fe77
LudmilaS <ludmila.https@gmail.com>
2023-01-16 11:20:13 -0300

    teste

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ server.get("/messages", async (request, response) => {
   console.log("get messages")
 
   const limit = parseInt(request.query.limit)
-  if (limit && (isNaN(limit) || limit < 1)) {
+  if (!limit && (isNaN(limit) || limit < 1)) {
     return response.status(422).send('Unprocessable Entity')
   }
 
